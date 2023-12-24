40bbf6f81f17faf333cb78c7dd83251e5888aa27
Akram <akr232009@gmail.com>
2023-12-24 22:07:09 +0600

    edit header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,38 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';g
+import {Navbar, Nav, FormControl, Container, Form, Button} from 'react-bootstrap'
+import * as PropTypes from "prop-types";
 
-class Header extends Component {
-    render() {
-
-        return (
-            <div>
-                
-            </div>
-        );
-    }
+function Containter(props) {
+    return null;
 }
 
-Header.propTypes = {};
+Containter.propTypes = {children: PropTypes.node};
+
+class Header extends Component {
+    logo;
+    render() {
+        return (
+            <Navbar fixed="top" collapseOnSelect expand="md" bg="light" variant="light">
+                <Containter>
+                    <Navbar.Brand href="/">
+                        <img
+                            src={this.logo}
+                            height="30"
+                            weight="30"
+                            className="d-inline-block align-top"
+                            alt="Logo"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav"/>
+                    <Nav className="mr-auto"/>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/">Our Team</Nav.Link>
+                    <Nav.Link href="/">Gallery</Nav.Link>
+                </Containter>
+            </Navbar>
+        )
+    }
+}
 
 export default Header;
