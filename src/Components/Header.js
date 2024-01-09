c2b6f71d81bf603e60fd187b9b1a944c7fa158d0
Akram <akr232009@gmail.com>
2024-01-09 20:18:56 +0600

    add logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,37 +1,36 @@
 import React, {Component} from 'react';
-import {Navbar, Nav, FormControl, Container, Form, Button} from 'react-bootstrap'
+import {Navbar, Nav, Container, FormControl, Form, Button} from 'react-bootstrap'
 import * as PropTypes from "prop-types";
+import logo from "./logo.svg";
 
-function Containter(props) {
-    return null;
-}
-
-Containter.propTypes = {children: PropTypes.node};
+Container.propTypes = {children: PropTypes.node};
 
 class Header extends Component {
-    logo;
     render() {
         return (
             <Navbar fixed="top" collapseOnSelect expand="md" bg="light" variant="light">
-                <Containter>
+                <Container>
                     <Navbar.Brand href="/">
                         <img
-                            src={this.logo}
-                            height="30"
-                            weight="30"
+                            src={logo}
+                            height="70"
+                            weight="70"
                             className="d-inline-block align-top"
                             alt="Logo"
                         />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                     <Navbar.Collapse id="responsive-navbar-nav"/>
-                    <Nav className="mr-auto"/>
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/">Our Team</Nav.Link>
-                    <Nav.Link href="/">Gallery</Nav.Link>
-                </Containter>
+                    <Nav className="mr-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/">Our Team</Nav.Link>
+                        <Nav.Link href="/">Gallery</Nav.Link>
+                    </Nav>
+                </Container>
             </Navbar>
         )
+            ;
+
     }
 }
 
